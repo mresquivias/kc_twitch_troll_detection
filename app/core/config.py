7c75d207093531083392b67ac3530c021be46f49
Andrés Esquivias <40606211+mresquivias@users.noreply.github.com>
2021-07-03 13:30:26 +0200

    New name to the app

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 import os
 
 APP_VERSION = "0.0.1"
-APP_NAME = "Text Sentiment Analysis Prediction"
+APP_NAME = "Twitch Troll Comment Detection"
 API_PREFIX = "/api"
 
 IS_DEBUG = os.getenv("IS_DEBUG", False)
